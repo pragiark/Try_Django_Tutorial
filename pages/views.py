0b8d961d377867ce4bf188521469e3328854f253
pragiark <pragiark@wp.pl>
2020-05-05 18:10:07 +0200

    add context record

--- pages/views.py
+++ pages/views.py
@@ -14,10 +14,11 @@ def contact_view(request, *args, **kwargs):
 
 def about_view(request, *args, **kwargs):
     my_context = {
-        "my_text": "This is about as",
+        "title": "this is about as aa",
         "this is true": True,
         "my_number": 7,
-        "my_list": [7, 77, 777, 7777, "ABC"]
+        "my_list": [7, 77, 777, 7777, "ABC"],
+        "my_html": "<h1>Hello world</h1>"
     }
     return render(request, "about.html", my_context)
 
